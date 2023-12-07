2ae6a258f51c1cf79537a70d8603573d16586147
Epsilon85083 <98786852+Epsilon85083@users.noreply.github.com>
2023-12-06 17:58:22 -0700

    Delete testing directory
    No longer needed with the implementation of basic setup.

--- testing/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module login {
-    requires javafx.fxml;
-    requires transitive javafx.controls;
-    exports login;
-    opens login to javafx.graphics, javafx.fxml;
-    exports application;
-    opens application to javafx.graphics, javafx.fxml;
-}
